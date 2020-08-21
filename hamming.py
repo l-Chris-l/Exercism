6d3afead0696e996c34d30c1f0dcd1e6219ec400
l-Chris-l <c.ckendall@aol.com>
2020-08-20 23:34:57 -0400

    Add hamming.py

---
+++ hamming.py
@@ -0,0 +1,20 @@
+leftStrand = input('Please enter first DNA strand: ').upper()
+rightStrand = input('Please enter second DNA strand: ').upper()
+
+def check(leftStrand, rightStrand):
+    differences = 0
+    if len(leftStrand) != len(rightStrand):
+        raise Exception('Strand lengths don\'t match')
+
+    for x, y in zip(leftStrand, rightStrand):
+            if x != y:
+                differences += 1
+            
+    print(differences)
+
+
+
+
+check(leftStrand, rightStrand)
+
+
